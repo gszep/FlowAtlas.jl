bf5d4210d789b3584aa4a1b7e5c792312c1b6166
gszep <gregory.szep@gmail.com>
2020-09-13 00:47:36 +0200

    validated multilabels: success

--- utils/workspace.py
+++ utils/workspace.py
@@ -1,12 +1,15 @@
 from xml.etree.cElementTree import ElementTree
-from networkx import DiGraph,draw,draw_networkx_edges
+from networkx import DiGraph,draw,draw_networkx_edges,set_node_attributes
 from pathlib import Path
 
-from FlowCytometryTools import PolyGate
-from pandas import DataFrame
-from numpy import array,arcsinh
+from matplotlib.patches import Polygon,PathPatch
+import fcsparser
 
-from matplotlib.pyplot import figure,fill,show,title,subplot,axes
+from pandas import DataFrame,MultiIndex,SparseDtype,concat
+from numpy import array,unique,roll,any
+from re import search
+
+from matplotlib.pyplot import figure,show,subplot,axes
 from networkx.drawing.nx_pydot import graphviz_layout
 
 class Workspace(object):
@@ -19,16 +22,21 @@ class Workspace(object):
 		"datatypes" : "http://www.isac-net.org/std/Gating-ML/v2.0/datatypes"
 	}
 
-	def __init__(self, path=str, uri_parser = None):
+	def __init__(self, path=str, channels={}, condition_parser = lambda x : {}):
 		
-		# parse metadata from samples
+		# parse samples from workspace
 		parent_dir = str(Path(path).parent)
-		self.datasets = {}
-		for sample in ElementTree(file=path).find('SampleList') :
+		samples,metadatas = [],[]
 
-			# parse channel and gating information
-			channels = self.__channels__(sample)
-			gating = self.__gating__(sample)
+		# record dimensions for verification
+		num_channels,num_labels = [],[]
+		ncells = 0
+
+		# format data for multicolumns
+		for key,value in channels.items() :
+			channels[key] = ('data',value)
+
+		for sample in ElementTree(file=path).find('SampleList') :
 	
 			# truncate paths to parent folder level
 			uri = sample.find('DataSet').get('uri')
@@ -37,52 +45,71 @@ class Workspace(object):
 			truncate_index = path_parts.index(parent_dir)
 			uri = str(Path(*path_parts[truncate_index:]))
 
-			# record metadata per sample
-			metadata = { 'uri': uri, 'channels': channels, 'gating': gating }
+			############################################################ import data
+			_,data = fcsparser.parse(path=uri,channel_naming='$PnN',dtype='float64')
 
-			if uri_parser == None : 
-				self.datasets[uri] = metadata
-			else : # custom parser
-				self.datasets[uri_parser(uri)] = metadata
+			# get labels from gating
+			gating = self.__gating__(sample)
+			labels = self.apply( data, gating )
 
-		self.ids = list(self.datasets.keys())
+			####################################### rename channels according to provided map
+			data.rename(columns=channels,inplace=True)
+			set_node_attributes(gating,{ id: [ channels[key][-1] for key in gating.nodes[id]['channels'] ] for id in gating },'channels')
 
+			metadata = DataFrame({'gating':[gating]})
+			data = data.join(labels) # join labels and data to one frame
+			data.columns = MultiIndex.from_tuples(data.columns)
 
-	def __channels__(self, sample) :
-		'''parse channel-marker pairs with transformation lambdas'''
-		parameters,channels = {},{}
+			###################################### use conditions as multi-index
+			for condition,value in condition_parser(uri).items() :
+				data[condition] = value
+				metadata[condition] = value
 
-		for keyword in sample.find('Keywords') :
-			name = keyword.get('name')
+			data.index.name = 'cell'
+			metadata.set_index( list(condition_parser(uri).keys()), inplace=True )
+			data.set_index( list(condition_parser(uri).keys()), inplace=True, append=True )
+			data.index = data.index.reorder_levels(roll(data.index.names,-1))
 
-			if name == '$PAR' : # get number of channels
-				n_parameters = int(keyword.get('value'))
+			metadatas.append(metadata)
+			samples.append(data)
 
-			elif '$P' == name[:2] : # extract channel/maker names
-				parameters[name] = keyword.get('value')
+			######################################## verification stats
+			cells,nchannels = data.shape
+			cells,nlabels = labels.shape
 
-		for n in range(1,n_parameters+1) : # match channel-marker pairs
-			channels[parameters['$P'+str(n)+'N']] = { 'marker': parameters['$P'+str(n)+'S'] }
+			num_channels.append(nchannels)
+			num_labels.append(nlabels)
+			ncells += cells
 
-		# attach transfromation info
-		return self.__transformations__(sample, channels)
+		self.frame = concat(samples,copy=False)
+		self.metadata = concat(metadatas,copy=False)
 
+		############################## converting indexes to category types for efficiency
+		for level in range(self.frame.index.nlevels-1) :
+			self.frame.index.set_levels(
+				self.frame.index.levels[level].astype('category'),
+				level=level, inplace=True)
 
-	def __transformations__(self, sample, channels) :
-		'''parse transformation as lambda functions'''
+		for level in range(self.frame.columns.nlevels) :
+			self.frame.columns.set_levels(
+				self.frame.columns.levels[level].astype('category'),
+				level=level, inplace=True)
 
-		for transformation in sample.find('Transformations') :
-			channel = transformation.find("{%s}parameter"%self.namespaces['datatypes']).get('{%s}name'%self.namespaces['datatypes'])
-			attributes = {}
+		for level in range(self.metadata.index.nlevels) :
+			self.metadata.index.set_levels(
+				self.metadata.index.levels[level].astype('category'),
+				level=level, inplace=True)
 
-			# parse transformation information
-			name = transformation.tag.replace('{'+self.namespaces['transformations']+'}','')
-			for key,value in transformation.attrib.items() :
-				attributes[key.replace('{'+self.namespaces['transformations']+'}','')] = float(value)
+		self.frame.sort_index(inplace=True,level=range(self.frame.index.nlevels-1))
+		self.metadata.sort_index(inplace=True)
+		self.index = self.metadata.index
 
-			channels[channel]['transformation'] = self.transform(name,attributes)
+		############################################################################ verifications
+		assert ncells == self.frame.shape[0], 'cells missing after concatination'
+		assert len(unique(num_channels)) == 1, 'different number of channels per sample'
 
-		return channels
+		assert len(unique(num_labels)) == 1, 'different number of populations per sample'
+		assert self.frame.isnull().sum().sum() == 0, 'missing data after concatination; check channel names'
 
 
 	def __gating__(self, sample) :
@@ -90,10 +117,11 @@ class Workspace(object):
 
 		# extract gating strategy
 		populations = list(sample.iter('Population'))
-		graph = DiGraph() if len(populations)>0 else None
+		assert len(populations)>0, 'gating strategy not found for {}'.format(sample)
+		graph = DiGraph()
 
 		for population in populations :
-			gate_name = population.get('name')
+			name = '__gate__'+population.get('name')
 			Gate = population.find('Gate')
 
 			id = Gate.get("{%s}id" % self.namespaces['gating'])
@@ -103,128 +131,118 @@ class Workspace(object):
 				assert 'PolygonGate' in str(gate), '{} not supported'.format(
 					str(gate).replace('{'+self.namespaces['gating']+'}','gating:'))
 				
-				gate_channels = []
+				channels = []
 				for dimension in gate.findall('gating:dimension/datatypes:fcs-dimension',self.namespaces) :
-					gate_channels.append( dimension.get("{%s}name" % self.namespaces['datatypes']) )
+					channels.append( dimension.get("{%s}name" % self.namespaces['datatypes']) )
 
 				vertexes = []
 				for vertex in gate.findall('gating:vertex/gating:coordinate',self.namespaces) :
 					vertexes.append( vertex.get("{%s}value" % self.namespaces['datatypes']) )
-				
-				vertexes = array(vertexes).astype(float).reshape(-1,2)
-				vertexes = list(zip(*vertexes.T))
 
-				graph.add_node( id, parent_id=parent_id,
-					gate=PolyGate(vertexes, gate_channels, name=str(gate_name), region='in'), gate_name=gate_name )
+				vertexes = array(vertexes).astype(float).reshape(-1,2)
+				graph.add_node( id, parent_id=parent_id, gate_name=name, channels=channels,
+					gate = Polygon(vertexes,fill=False,edgecolor='gold') )
+
 				if parent_id is not None :
 					graph.add_edge(parent_id,id)
 
 		return graph
 
 
-	def transform(self, name, attributes):
-		'''return lamba function from attributes'''
+	def gate(self, data, gating, id):
+		'''apply gate to DataFrame from node[id]'''
 
-		length = attributes['length']
-		maxRange = attributes['maxRange']
+		node = gating.nodes[id]
+		name,channels,gate = node['gate_name'],node['channels'],node['gate']
 
-		if name == 'fasinh' :
-			assert maxRange == attributes['T']
-			negative_decades = attributes['A']
-			attributes['M'] # no idea what this parameter does
+		# current gate 
+		data[name] = gate.get_path().contains_points(data.filter(channels))
 
-			width_basis = abs(attributes['W'])
+		if gating.in_degree(id)>0 : # and parents
+			parent = array([ data[gating.nodes[parent]['gate_name']] for parent in gating.predecessors(id) ])
+			data[name] &= any(parent,axis=0)
 
-		elif name == 'biex' :
-			negative_decades = attributes['neg']
-			attributes['pos'] # no idea what this parameter does
-			width_basis = abs(attributes['width'])
+		# iterate through children
+		for child in gating.successors(id) :
+			data = self.gate(data,gating,child)
 
-		else :
-			raise NotImplementedError('{} transformation not implemented'.format(name))
-		
-		return lambda x : arcsinh(array(x)/width_basis)
+		return data
 
 
-	def apply(self, FCMeasurement, id):
-		'''apply gate to FCMeasurement object from node[id]'''
+	def apply(self, data, gating):
+		'''apply gating tree DataFrame and return boolean one-hot array'''
 
-		node = self.graph.nodes[id]
-		gate = node['gate']
-
-		# match channel names between FCMeasurement object and GatingML file
-		if '$PnS' in FCMeasurement.channels.columns and set(gate.channels).issubset(set(FCMeasurement.channels['$PnN'])):
-			gate.channels = FCMeasurement.channels.set_index('$PnN').loc[gate.channels]['$PnS']
-
-		# perform gate
-		FCMeasurement = FCMeasurement.gate(gate)
-
-		# continue performing gates if node has parents
-		if self.graph.in_degree(id)==1 :
-			return self.apply(FCMeasurement,node['parent_id'])
-		else :
-			return FCMeasurement
-
-
-	def get_labels(self, FCMeasurement, sample_id):
-		'''apply gating tree to FCMeasurement object and return row-level labels'''
-		
-		self.graph = self.datasets[sample_id]['gating']
-		labels = DataFrame( {'label': [[]] }, index=FCMeasurement.data.index)
-
-		if self.graph == None :
-			return labels.label
-
-		population_ids = ( id for id in self.graph if self.graph.out_degree(id)==0 and self.graph.in_degree(id)==1 )
-		for id in population_ids :
+		roots = [ id for id in gating if gating.in_degree(id)==0 ]
+		for id in roots :
+			data = self.gate(data,gating,id)
 			
-			label = self.graph.nodes[id]['gate_name']
-			label_index = self.apply(FCMeasurement.copy(),id).data.index
-			labels.loc[label_index,'label'] = labels.loc[label_index,'label'].apply(lambda x: x+[label])
-			
-		return labels.label
+		labels = data.filter([ gating.nodes[id]['gate_name'] for id in gating ])
+		data.drop( columns = labels.columns, inplace=True)
+
+		labels.columns = labels.columns.str.lstrip('__gate__')
+		labels.columns = [ ('labels',column) for column in labels.columns ]
+		return labels   #.astype(SparseDtype(bool, fill_value=False))
 
 				
-	def show(self, sample_id, plot_size = 0.05):
+	def show(self, id, xlim=(-1e3,1e5), ylim=(-1e3,1e5),
+			 linthresh=1e3, linscale=0.5, plot_size = 0.08):
 		'''display gating heirarchy'''
 
-		metadata = self.datasets[sample_id]
-		positions = graphviz_layout(metadata['gating'], prog='dot')
+		data,metadata =  self.frame.xs(id), self.metadata.xs(id)
+		positions = graphviz_layout(metadata.gating, prog='dot')
 
-		fig = figure(figsize=(20,20))
-		fig.suptitle(metadata['uri'], fontsize=16)
+		fig = figure(figsize=(30,30))
+		fig.suptitle('file_index = {}'.format(id), fontsize=16)
 		ax = subplot(111)
 
 		transform =  fig.transFigure.inverted().transform
-		draw_networkx_edges(metadata['gating'], positions, ax=ax,
-				edge_color='r', width=5, arrowsize=1)
+		draw_networkx_edges(metadata.gating, positions, ax=ax,
+				edge_color='gold', width=5, arrowsize=1)
 
-		for id in metadata['gating']:
+		for id in metadata.gating:
 
-			node = metadata['gating'].nodes[id]
+			node = metadata.gating.nodes[id]
+			xchannel,ychannel = node['channels']
+
+			name = node['gate_name'].lstrip('__gate__')
 			gate = node['gate']
-
-			xchannel,ychannel = gate.channels
-			xchannel = metadata['channels'][xchannel]
-			ychannel = metadata['channels'][ychannel]
 
 			x,y = transform(ax.transData.transform(positions[id])) # axes coordinates
 			gate_axes = axes([x-plot_size/2.0,y-plot_size/2.0,plot_size,plot_size])
+
 			gate_axes.set_aspect('equal')
+			gate_axes.set_title(name)
 
-			gate_axes.set_title(node['gate_name'])
-			gate_axes.set_xlim(-4,8); gate_axes.set_ylim(-4,8)
+			################################################################### gate
+			gate_axes.add_patch(gate)
 
-			x,y = zip(*gate.vert) # display gate
-			fill(xchannel['transformation'](x),ychannel['transformation'](y),fill=False,linewidth=3,color='orange')
+			################################################################### populations
+			if metadata.gating.in_degree(id)==0 : # whole population
+				gate_axes.scatter(data.data[xchannel],data.data[ychannel],color='gray',s=1)
 
-			gate_axes.set_xlabel(xchannel['marker'])
-			gate_axes.set_ylabel(ychannel['marker'])
+			else : # parent populations
+				for parent in metadata.gating.predecessors(id) :
+					mask = data.labels[metadata.gating.nodes[parent]['gate_name'].lstrip('__gate__')]
+					gate_axes.scatter(data.data[xchannel][mask],data.data[ychannel][mask],color='gray',s=1)
+
+			# gated population
+			mask = data.labels[name]
+			gate_axes.scatter(data.data[xchannel][mask],data.data[ychannel][mask],color='gold',s=1)
+
+			gate_axes.set_xscale('symlog',linthresh=linthresh,linscale=linscale)
+			gate_axes.set_yscale('symlog',linthresh=linthresh,linscale=linscale)
+
+			gate_axes.set_xlim(*xlim)
+			gate_axes.set_ylim(*ylim)
+
+			gate_axes.set_xticks([])
+			gate_axes.set_yticks([])
+
+			gate_axes.set_xlabel(xchannel)
+			gate_axes.set_ylabel(ychannel)
 
 			gate_axes.xaxis.label.set_size(10)
 			gate_axes.yaxis.label.set_size(10)
-
-			gate_axes.tick_params(axis='both', which='major', labelsize=6)
 
 		ax.axis('off')
 		show()
