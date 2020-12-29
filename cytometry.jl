708603f806f90375a7f27f840d83fd305af9e4ac
gszep <gregory.szep@gmail.com>
2020-12-29 21:40:15 +0100

    dynamic violin plots

--- cytometry.jl
+++ cytometry.jl
@@ -40,6 +40,9 @@ begin
 	include("lib/utils.jl")
 	using GigaSOM,Images,GigaScatter
 	using MetaGraphs,DataFrames
+	
+	using StaticArrays,PolygonOps
+	using KernelDensity
 end
 
 # ╔═╡ ba294c1c-43ab-11eb-0695-1147232dc62a
@@ -102,18 +105,6 @@ begin
 				         "CD127- CD25+","non-Tregs"]) )
 end
 
-# ╔═╡ 48d505a4-469b-11eb-35b5-397a9a5498c2
-begin ######## convert one-hot encoding to string labels
-# 	labels.cell, fcsdata.cell = 1:size(fcsdata,1), 1:size(fcsdata,1)
-# 	populations = stack(labels,Not(:cell),:cell,variable_name=:label)
-	
-# 	filter!(:value => x->x, populations)
-# 	select!(populations,[:cell,:label])
-	
-# 	categorical!(populations,:label,compress=true)
-# 	sort!(populations,:cell)
-end
-
 # ╔═╡ 20596a96-4708-11eb-0b61-539eba64e3fd
 begin
 	#################### train self-organised map
@@ -137,83 +128,129 @@ We can explore the clusters in the embedded space given by `EmbedSOM`. We can co
 # ╔═╡ aae5b128-436a-11eb-092b-0fc350961437
 begin ###################################################### scene construction
 
-	channels = names(fcsdata)
+	channels, populations = names(fcsdata),names(labels)
 	channel = Observable(first(channels))
 	fluorescence = Observable(false)
 	
-	populations = names(labels)
-	colors = Dict([ name=> Observable("#EEEEEE") for name ∈ populations ])
+	########################################### transformation to unit interval
+	function channelTransform(x::AbstractArray)
+		return ( x .- minimum(x) )/( maximum(x) - minimum(x) )
+	end
 	
+	scaled = combine(fcsdata,[ col => channelTransform => col
+			for col ∈ names(fcsdata) ] )
+	
+	################################################## colormaps
+	colors = Dict([ name=> Observable("#EEEEEE") for name ∈ populations ])
+	fluorescenceMap = Dict([
+		name => @lift( colorview(RGBA, expressionColors(
+						
+			scaled[labels[!,name],$channel], $fluorescence ?
+			expressionPalette(10,alpha=1/4) : 
+						[parse(RGBA,$(colors[name]))]
+		)))
+	for name ∈ populations ])
+	
+	####################################################
 	scene, layout = layoutscene(resolution = (500,500))
-	ax = layout[1,1] = LAxis(scene, 
-		title="Self-organised Map Embedding")
+	ax = layout[1,1] = LAxis(scene,title="Self-organised Map Embedding")
 
 	empty!(scene.events.mousedrag.listeners)
 	mouseevents = MakieLayout.addmouseevents!(ax.scene)
 	
+	#################################################### scatterplot	
 	for name ∈ populations
-		scatter!( ax, embedding[labels[!,name],:], markersize=1,
-			color=@lift( $fluorescence ? "#ABCDEF" : $(colors[name]) ) )
+		scatter!( ax, embedding[labels[!,name],:],
+			color=fluorescenceMap[name], markersize=1 )
 	end
-		
-	# @lift( colorview(RGBA, expressionColors(
-	# 	  scaleNorm(fcsdata[:,$channel]), expressionPalette(10, alpha=1/4)
-	# 		)))
 	
-
-
-	# leg = layout[1, end+1] = LLegend(scene, 
-	# [line1, scat1, line2, scat2], ["True", "Measured", "True", "Measured"])
-
-	#layout[1,2] = LColorbar(scene,sc,width=10,height=Relative(1))
-
+	#################################################### polygon selection
+	polygon = Node([ SVector(0.0,0.0),SVector(5.0,0.0),SVector(5.0,5.0) ])
+	closedPolygon = @lift([$polygon; [first($polygon)]])
+	lines!(ax, closedPolygon, color=RGBA(1/2,1,1/2,1))
 	
-# 	###################################################### bind input events	
-# 	on(scene.events.keyboardbuttons) do button
-		
-# 		if ispressed(button, Keyboard._1)
-# 			μ₁[] = mouseevents.obs.val.data
-			
-# 		elseif ispressed(button, Keyboard._2)
-# 			μ₂[] = mouseevents.obs.val.data
-# 		end
-# 	end
-	
-    RecordEvents( scene, "output" )
-	println("Rendering Done")
+	on(ax.scene.events.mousebuttons) do buttons
+	   if ispressed(ax.scene, Mouse.left)
+		   polygon[] = push!(polygon[], mouseevents.obs.val.data)
+	   end
+	   return
+
+	end
+	nothing
 end
 
 # ╔═╡ 7cdadea8-4715-11eb-220c-475f60a98543
 begin
 	JSServe.with_session() do session, request
-		return DOM.div( style,
+		return DOM.div( style, DOM.br(), DOM.br(),
 			
-			########################## embedding scatterplot
-			DOM.div(scene),
-			
-			######################################### population color interactions
-			DOM.div( DOM.label("Populations"), DOM.br(),
-			[ ( DOM.input(type="color",name=name,id=name,value=colors[name].val,
-								
-					onchange=js"update_obs(($colors)[name],this.value)"),
-			    DOM.label(name), DOM.br() ) for name ∈ populations ]),
-		
-			
-			######################################## channel intensity
-			DOM.div( DOM.input(type="checkbox",
-				onchange=js"update_obs($fluorescence,this.checked);"),
-			
-			DOM.label("Fluorescence Intensity"), DOM.select(DOM.option.(channels),
-			onchange=js"update_obs($channel,this.options[this.selectedIndex].text)"),
-			)
+		######################################## channel intensity
+		DOM.div( DOM.input(type="checkbox",
+			onchange=js"update_obs($fluorescence,this.checked);"),
+
+		DOM.label("Fluorescence Intensity"), DOM.select(DOM.option.(channels),
+		onchange=js"update_obs($channel,this.options[this.selectedIndex].text)")),
+
+		########################## embedding scatterplot
+		DOM.div(scene),
+
+		######################################### population color interactions
+		DOM.div( DOM.label("Populations"), DOM.br(),
+		[ ( DOM.input(type="color",name=name,id=name,value=colors[name].val,
+
+				onchange=js"update_obs(($colors)[name],this.value)"),
+			DOM.label(name), DOM.br() ) for name ∈ populations ]),
 		)
 	end
 end
 
-# ╔═╡ 1973cde6-4523-11eb-3739-f9a9f97f197e
+# ╔═╡ 24a08ba4-4a15-11eb-1107-2dc357228e44
 md"""
-#### Cluster Exploration
-In progress....
+* constran zoom so that user does not fly off plot
+* sliders for zoom, markersize, markeralpha
+"""
+
+# ╔═╡ 9be70f5e-4a14-11eb-3635-bd9fef2ea09a
+md"""
+Fluorescence distributions of selected populations can be compared across conditions. The conditions are extracted from groups defined in the FlowJo workspace.
+"""
+
+# ╔═╡ b2a63c7a-49f7-11eb-300d-b7e283639a39
+begin
+	#################################################### density estimation	
+	selected = @lift(fcsdata[ map( (x,y)->inpolygon(SVector(x,y),
+		$closedPolygon; in=true,on=false,out=false),
+		embedding[:,1], embedding[:,2] ),:])
+	
+	channelRange = range(-2,7,length=50)
+	density(x::AbstractVector) = map(SVector,kde(x,channelRange).density,channelRange)
+	densities = @lift(combine( $selected, [ x => density => x for x ∈ channels ]) )
+
+	################################################################# violin plot
+	violins, violinsLayout = layoutscene(resolution = (650,300))
+	violinsAx = violinsLayout[1,1] = LAxis(violins,title="Fluorescence")
+	
+	for (k,channel) ∈ enumerate(channels)
+		maxDensity = @lift( maximum(x->x[1], $densities[!,channel]) )
+		
+		lines!(violinsAx, @lift(map( x-> x./SVector(2.5*$maxDensity,1)+SVector(k,0),
+			$densities[!,channel]) ), color=RGBA(0,1,0,0.2))
+		
+		lines!(violinsAx, @lift(map( x-> x./SVector(-2.5*$maxDensity,1)+SVector(k,0), 
+			$densities[!,channel])), color=RGBA(0,1,0,0.2))
+	end
+	
+	violinsAx.xticks = (1:length(channels), channels)
+	violinsAx.xticklabelrotation = Float32(π/2)
+	violinsAx.xticklabelalign = (:top,:center)
+	violins
+end
+
+# ╔═╡ 0867a38c-4a15-11eb-0583-21b96f8009c3
+md"""
+* dropdown menues for filter and group by (tissue,label,patient)
+* displaying stacked violin plots
+* category columns must be parsed from workspace groups
 """
 
 # ╔═╡ Cell order:
@@ -227,9 +264,11 @@ In progress....
 # ╟─12e279d2-4477-11eb-0f4e-510c1329a935
 # ╟─199c6556-4525-11eb-154b-034d1b0e0692
 # ╠═81118ba6-4522-11eb-3de4-7f385efb6375
-# ╠═48d505a4-469b-11eb-35b5-397a9a5498c2
 # ╟─20596a96-4708-11eb-0b61-539eba64e3fd
 # ╟─0b758ce0-4528-11eb-1df4-e97707ec4f1c
-# ╠═aae5b128-436a-11eb-092b-0fc350961437
-# ╠═7cdadea8-4715-11eb-220c-475f60a98543
-# ╟─1973cde6-4523-11eb-3739-f9a9f97f197e
+# ╟─aae5b128-436a-11eb-092b-0fc350961437
+# ╟─7cdadea8-4715-11eb-220c-475f60a98543
+# ╟─24a08ba4-4a15-11eb-1107-2dc357228e44
+# ╟─9be70f5e-4a14-11eb-3635-bd9fef2ea09a
+# ╟─b2a63c7a-49f7-11eb-300d-b7e283639a39
+# ╟─0867a38c-4a15-11eb-0583-21b96f8009c3
