c014950f34ba78b28cc9cbfe6f523ba7555e0106
gszep <gregory.szep@gmail.com>
2021-01-03 12:06:31 +0100

    helpful compilation across platforms

--- run.jl
+++ run.jl
@@ -1,2 +1,10 @@
-using Pluto
-Pluto.run(notebook="cytometry.jl",sysimage="cytometry.so")
+using Pkg
+Pkg.activate(".")
+
+if isfile("cytometry.so")
+    using Pluto
+    Pluto.run(notebook="cytometry.jl",sysimage="cytometry.so")
+else
+    println("Running app for the first time requires building cytometry.so\nThis may take minutes, feel free to grab a cuppa tea ;)")
+    include("build/sysimage.jl")
+end
